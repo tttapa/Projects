2f32fad78a99f2a4929621091d950e2f1fd59416
tttapa <pieter.pas@outlook.com>
2017-08-11 10:49:53 +0300

    Add PHP scripts

---
+++ ESP8266/HTTP/HTTP-POST-char-array/PHP/db-connect.php
@@ -0,0 +1,14 @@
+<?php
+$database = "IoT";  // name of the database
+$username = "root";  // MySQL username
+$password = "password123";  // MySQL password
+
+try {  // connect to database
+  $db = new PDO("mysql:host=localhost;dbname=${database};charset=utf8mb4", 
+    $username, $password, 
+    array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+} catch(PDOException $ex) {
+  http_response_code(500);
+  echo "Unable to connect to the database";
+  error_log($ex->getMessage());
+}?>
