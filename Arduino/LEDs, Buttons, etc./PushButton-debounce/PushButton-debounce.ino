503aef6b836ba39239e2e0c5de6adf39fe6d8f81
Pieter <pieter.pas@outlook.com>
2017-11-13 00:27:04 +0100

    Add PushButton-debounce example

---
+++ Arduino/LEDs, Buttons, etc./PushButton-debounce/PushButton-debounce.ino
@@ -0,0 +1,48 @@
+class PushButton
+{
+  public:
+    PushButton(uint8_t pin) // Constructor (executes when a PushButton object is created)
+      : pin(pin) { // remember the push button pin
+      pinMode(pin, INPUT_PULLUP); // enable the internal pull-up resistor
+    };
+    bool isPressed() // read the button state check if the button has been pressed, debounce the button as well
+    {
+      bool pressed = false;
+      bool state = digitalRead(pin);               // read the button's state
+      int8_t stateChange = state - previousState;  // calculate the state change since last time
+
+      if (stateChange == falling) { // If the button is pressed (went from high to low)
+        if (millis() - previousBounceTime > debounceTime) { // check if the time since the last bounce is higher than the threshold
+          pressed = true; // the button is pressed
+        }
+      }
+      if (stateChange == rising) { // if the button is released or bounces
+        previousBounceTime = millis(); // remember when this happened
+      }
+
+      previousState = state; // remember the current state
+      return pressed; // return true if the button was pressed and didn't bounce
+    };
+  private:
+    uint8_t pin;
+    bool previousState = HIGH;
+    unsigned long previousBounceTime = 0;
+
+    const unsigned long debounceTime = 25;
+    const int8_t rising = HIGH - LOW;
+    const int8_t falling = LOW - HIGH;
+};
+
+// -------------------------------------------------------------------------------------------------------------------------------- //
+
+PushButton button = { 2 };  // Create a new PushButton object on pin 2
+
+void setup() {
+    pinMode(LED_BUILTIN, OUTPUT);  // Set the built-in LED (pin 13) to output mode
+}
+
+void loop() {
+    if (button.isPressed()) {  // If the button is pressed
+        digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));  // Toggle the LED state
+    }
+}
