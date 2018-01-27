d72ada6a5e8514693fbc958c05f7e7c3af463fe0
tttapa <pieter.pas@outlook.com>
2018-01-27 22:02:38 +0100

    Update db-connect.php

--- ESP8266/HTTP/HTTP-POST-char-array/PHP/db-connect.php
+++ ESP8266/HTTP/HTTP-POST-char-array/PHP/db-connect.php
@@ -6,7 +6,7 @@ $password = "password123";  // MySQL password
 try {  // connect to database
   $db = new PDO("mysql:host=localhost;dbname=${database};charset=utf8mb4", 
     $username, $password, 
-    array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    array(PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 } catch(PDOException $ex) {
   http_response_code(500);
   error_log($ex->getMessage());
