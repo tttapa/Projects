28f2947e014cd7fc34d274299a9df8dd9e44a117
Pieter Pas <pieter.pas@outlook.com>
2019-11-16 21:18:30 +0100

    Improve PushButton-debounce

--- Arduino/LEDs, Buttons, etc./PushButton-debounce/PushButton-debounce.ino
+++ Arduino/LEDs, Buttons, etc./PushButton-debounce/PushButton-debounce.ino
@@ -1,49 +1,78 @@
+/*
+ * Example sketch that defines a class for debouncing push buttons.
+ * It uses this class to toggle the built-in LED:
+ * 
+ * Connect a push button between ground and digital pin 2, then upload the 
+ * sketch, and push the button.
+ * If you press it once, the LED will turn on, if you press it again, the LED
+ * will turn off again.
+ * 
+ * To illustrate the problems with contact bounce, set the `debounceTime` to
+ * zero and observe how hard it is to get the LED to turn on/off.
+ */
+
 class PushButton {
-  public:
-    PushButton(uint8_t pin)  // Constructor (executes when a PushButton object is created)
-      : pin(pin) {  // remember the push button pin
-      pinMode(pin, INPUT_PULLUP); // enable the internal pull-up resistor
-    };
+ public:
+  /// Constructor (executes when a PushButton object is created).
+  PushButton(uint8_t pin)
+    : pin(pin) // remember the push button pin
+  {}
 
-    bool isPressed() {  // read the button state check if the button has been pressed, debounce the button as well
-      bool pressed = false;
-      bool state = digitalRead(pin);               // read the button's state
-      int8_t stateChange = state - previousState;  // calculate the state change since last time
+  /// Initialize: enable the internal pull-up resistor.
+  void begin() { pinMode(pin, INPUT_PULLUP); }
 
-      if (stateChange == falling) { // If the button is pressed (went from high to low)
-        if (millis() - previousBounceTime > debounceTime) { // check if the time since the last bounce is higher than the threshold
-          pressed = true; // the button is pressed
-        }
-      } else if (stateChange == rising) { // if the button is released or bounces
-        previousBounceTime = millis(); // remember when this happened
+  /// Read the button state check if the button has been pressed, debounce the
+  /// button, and return true on a falling edge (transition from not pressed
+  /// to pressed).
+  bool isPressed() {
+    bool pressed = false;
+    // read the button's state
+    bool state = digitalRead(pin);
+    // calculate the state change since last time
+    int8_t stateChange = state - previousState;
+
+    // If the button is pressed (went from high to low)
+    if (stateChange == falling) {
+      // check if the time since the last bounce is higher than the threshold
+      if (millis() - previousBounceTime >= debounceTime) {
+        pressed = true; // the button is pressed
       }
+    }
+    // if the button is released or bounces
+    else if (stateChange == rising) {
+      // remember when this happened
+      previousBounceTime = millis();
+    }
 
-      previousState = state; // remember the current state
-      return pressed; // return true if the button was pressed and didn't bounce
-    };
+    // remember the current state
+    previousState = state;
+    // return true if the button was pressed and didn't bounce
+    return pressed;
+  }
 
-  private:
-    uint8_t pin;
-    bool previousState = HIGH;
-    unsigned long previousBounceTime = 0;
+ private:
+  uint8_t pin;
+  bool previousState = HIGH;
+  unsigned long previousBounceTime = 0;
 
-    const static unsigned long debounceTime = 25; // milliseconds
-    const static int8_t rising = HIGH - LOW;
-    const static int8_t falling = LOW - HIGH;
+  constexpr static unsigned long debounceTime = 25; // milliseconds
+  constexpr static int8_t rising = HIGH - LOW;
+  constexpr static int8_t falling = LOW - HIGH;
 };
 
-// -------------------------------------------------------------------------------------------------------------------------------- //
+// :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: //
 
-PushButton button = { 2 };  // Create a new PushButton object on pin 2
+PushButton button = {2}; // Create a new PushButton object on pin 2
 
 void setup() {
-    pinMode(LED_BUILTIN, OUTPUT);  // Set the built-in LED (pin 13) to output mode
+  button.begin();
+  pinMode(LED_BUILTIN, OUTPUT); // Set the built-in LED (pin 13) to output mode
 }
 
 void loop() {
-    static bool LEDstate = LOW;
-    if (button.isPressed()) {  // If the button is pressed
-        LEDstate = !LEDstate;  // Flip the state of the LED
-        digitalWrite(LED_BUILTIN, LEDstate);  // Write the new state to the LED
-    }
+  static bool LEDstate = LOW;
+  if (button.isPressed()) {              // If the button is pressed
+    LEDstate = !LEDstate;                // Flip the state of the LED
+    digitalWrite(LED_BUILTIN, LEDstate); // Write the new state to the LED
+  }
 }
